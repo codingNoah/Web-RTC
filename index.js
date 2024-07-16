7b3a732cef0d5ea714e9df51f4b35cb9aeeee493
yenoh <tessemayenoh94@gmail.com>
2024-07-16 23:24:11 +0300

    Backend Cleanup

--- index.js
+++ index.js
@@ -80,8 +80,6 @@ io.on("connection", (socket) => {
   });
 
   socket.on("newOffer", (newOffer) => {
-    console.log("New Offer", socket.handshake.auth.username);
-    const username = socket.handshake.auth.username;
     offers.push({
       offererUserName: socket.handshake.auth.username,
       offer: newOffer,
@@ -95,28 +93,15 @@ io.on("connection", (socket) => {
   });
 
   socket.on("newAnswer", (offerObj, ackFunction) => {
-    console.log("---------------------------", "new answer");
-    console.log(
-      typeof offerObj.offererUserName,
-      offerObj.offererUserName,
-      typeof connectedSockets[0].username,
-      typeof connectedSockets[1].username
-    );
-    console.log(connectedSockets);
-    //emit this answer (offerObj) back to CLIENT1
-    //in order to do that, we need CLIENT1's socketid
     const socketToAnswer = connectedSockets.find((s) => {
-      //   console.log(s.username, s.socketId, s);
-
       return s.username == offerObj.offererUserName;
     });
     if (!socketToAnswer) {
       console.log("No matching socket");
       return;
     }
-    //we found the matching socket, so we can emit to it!
+
     const socketIdToAnswer = socketToAnswer.socketId;
-    //we find the offer to update so we can emit it
     const offerToUpdate = offers.find(
       (o) => o.offererUserName === offerObj.offererUserName
     );
@@ -124,12 +109,11 @@ io.on("connection", (socket) => {
       console.log("No OfferToUpdate");
       return;
     }
-    //send back to the answerer all the iceCandidates we have already collected       noah 4372.441721548841
+
     ackFunction(offerToUpdate.offerIceCandidates);
     offerToUpdate.answer = offerObj.answer;
     offerToUpdate.answererUserName = socket.handshake.auth.username;
-    //socket has a .to() which allows emiting to a "room"
-    //every socket has it's own room
+
     socket.to(socketIdToAnswer).emit("answerResponse", offerToUpdate);
   });
 
