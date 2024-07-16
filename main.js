75b41b6d1f8be8e17cf0e9a22a3dcedee11fa369
yenoh <tessemayenoh94@gmail.com>
2024-07-16 23:27:02 +0300

    FrontEnd clean up

--- main.js
+++ main.js
@@ -19,8 +19,6 @@ const socket = io("http://localhost:3003", {
 
 socket.on("connect", () => {
   console.log("Connected to server");
-  // Send a message to the server
-  // socket.send("Hello, server!");
 });
 
 const localVideo = document.getElementById("localVideo");
@@ -33,29 +31,21 @@ let didIOffer = false;
 
 socket.on("receivedIceCandidateFromServer", (iceCandidate) => {
   connection.addIceCandidate(iceCandidate);
-  console.log(
-    "======Added Ice Candidate======",
-    "receivedIceCandidateFromServer"
-  );
 });
 
 socket.on("availableOffers", (offers) => {
   const answerEl = document.querySelector("#answer");
   offers.forEach((o) => {
-    console.log(o);
     const newOfferEl = document.createElement("div");
     newOfferEl.innerHTML = `<button class="btn btn-success col-1">Answer ${o.offererUserName}</button>`;
     newOfferEl.addEventListener("click", () => answerOffer(o));
     answerEl.appendChild(newOfferEl);
   });
-
-  console.log("======Added An Offer======", "availableOffers");
 });
 
 socket.on("newOfferAwaiting", (offers) => {
   const answerEl = document.querySelector("#answer");
   offers.forEach((o) => {
-    console.log(o);
     const newOfferEl = document.createElement("div");
     newOfferEl.innerHTML = `<button class="btn btn-success col-1">Answer ${o.offererUserName}</button>`;
     newOfferEl.addEventListener("click", () => answerOffer(o));
@@ -67,26 +57,19 @@ socket.on("newOfferAwaiting", (offers) => {
 
 socket.on("answerResponse", (offerObj) => {
   connection.setRemoteDescription(offerObj.answer);
-  console.log(
-    "======setRemoteDescription answerResponse======",
-    "answerResponse"
-  );
 });
 
 const call = async () => {
-  // Getting user media
   await getUserMedia();
 
-  // Creating connection
   await createConnection();
 
   try {
-    // console.log("Creating offer....");
     const offer = await connection.createOffer();
 
     connection.setLocalDescription(offer);
     didIOffer = true;
-    socket.emit("newOffer", offer); //send offer to signalingServer
+    socket.emit("newOffer", offer);
   } catch (error) {
     console.log(error);
   }
@@ -98,8 +81,6 @@ const getUserMedia = async () => {
       audio: true,
       video: true,
     });
-
-    // console.log(stream);
 
     localVideo.srcObject = stream;
     localStream = stream;
@@ -120,7 +101,6 @@ const createConnection = async (offerObj) => {
     });
 
     connection.addEventListener("icecandidate", (e) => {
-      console.log("ice candidates found....");
       if (e.candidate) {
         socket.emit("sendIceCandidateToSignalingServer", {
           iceCandidate: e.candidate,
@@ -131,11 +111,8 @@ const createConnection = async (offerObj) => {
     });
 
     connection.addEventListener("track", (e) => {
-      console.log("Got a track from the other peer!! How excting");
-      console.log(e);
       e.streams[0].getTracks().forEach((track) => {
         remoteStream.addTrack(track, remoteStream);
-        console.log("Here's an exciting moment... fingers cross");
       });
     });
 
@@ -148,27 +125,17 @@ const createConnection = async (offerObj) => {
 };
 
 const answerOffer = async (offerObj) => {
-  console.log(offerObj);
   await getUserMedia();
   await createConnection(offerObj);
-  const answer = await connection.createAnswer({}); //just to make the docs happy
-  await connection.setLocalDescription(answer); //this is CLIENT2, and CLIENT2 uses the answer as the localDesc
-  console.log(offerObj);
-  console.log(answer);
-  // console.log(peerConnection.signalingState) //should be have-local-pranswer because CLIENT2 has set its local desc to it's answer (but it won't be)
-  //add the answer to the offerObj so the server knows which offer this is related to
+  const answer = await connection.createAnswer({});
+  await connection.setLocalDescription(answer);
+
   offerObj.answer = answer;
-  //emit the answer to the signaling server, so it can emit to CLIENT1
-  //expect a response from the server with the already existing ICE candidates
+
   const offerIceCandidates = await socket.emitWithAck("newAnswer", offerObj);
   offerIceCandidates.forEach((c) => {
     connection.addIceCandidate(c);
-    console.log("======Added Ice Candidate======");
   });
-  console.log(offerIceCandidates);
-  return;
 };
 
 document.querySelector("#call").addEventListener("click", call);
-
-// console.log("running...");
